def9c8fa18fb4705596402a9480eeba808229425
Rohith <gambol99@gmail.com>
2016-06-14 15:13:36 +0100

    - adding metadata to the types

--- types.go
+++ types.go
@@ -28,9 +28,9 @@ var (
 )
 
 var (
-	// SupportedAuthBackends is a list of supported auth backends
+	// SupportedAuthBackends is a list of supported auth backend's
 	SupportedAuthBackends = []string{"userpass", "ldap", "token", "appid", "github", "mfa", "tls"}
-	// SupportedBackendTypes is a list of supported secret backends
+	// SupportedBackendTypes is a list of supported secret backend's
 	SupportedBackendTypes = []string{
 		"aws", "generic", "pki", "transit",
 		"cassandra", "consul", "cubbyhole", "mysql",
@@ -51,8 +51,13 @@ type Config struct {
 	SkipTLSVerify bool
 }
 
+// Metadata defines additional attributes
+type Metadata map[string]string
+
 // Auth defined a authentication backend
 type Auth struct {
+	// Metadata defines additional attributes
+	Metadata Metadata `yaml:"metadata,omitempty" json:"metadata,omitempty"`
 	// Path is the path of the authentication backend
 	Path string `yaml:"path" json:"path" hcl:"path"`
 	// Type is the authentication type
@@ -65,6 +70,8 @@ type Auth struct {
 
 // Backend defined the type and configuration for a backend in vault
 type Backend struct {
+	// Metadata defines additional attributes
+	Metadata Metadata `yaml:"metadata,omitempty" json:"metadata,omitempty"`
 	// Path is the mountpoint for the mount
 	Path string `yaml:"path" json:"path" hcl:"path"`
 	// Description is the a description for the backend
@@ -81,6 +88,8 @@ type Backend struct {
 
 // Policy defines a vault policy
 type Policy struct {
+	// Metadata defines additional attributes
+	Metadata Metadata `yaml:"metadata,omitempty" json:"metadata,omitempty"`
 	// Name is the name of the policy
 	Name string `yaml:"name" json:"name" hcl:"name"`
 	// Path is a series of paths and their permissions
@@ -89,6 +98,8 @@ type Policy struct {
 
 // PolicyPermission represents a path permission
 type PolicyPermission struct {
+	// Metadata defines additional attributes
+	Metadata Metadata `yaml:"metadata,omitempty" json:"metadata,omitempty"`
 	// Policy is the permission
 	Policy string `yaml:"policy" json:"policy" hcl:"policy"`
 	// Capabilities
@@ -97,6 +108,8 @@ type PolicyPermission struct {
 
 // Secret defines a secret
 type Secret struct {
+	// Metadata defines additional attributes
+	Metadata Metadata `yaml:"metadata,omitempty" json:"metadata,omitempty"`
 	// Path is key for this secret
 	Path string `yaml:"path" json:"path" hcl:"path"`
 	// Values is a series of values associated to the secret
